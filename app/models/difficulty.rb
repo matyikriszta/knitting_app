67ebfa12c42697f31978fd2a07539f22a6366b35
Kriszta <matyi.kriszta@gmail.com>
2014-03-15 15:58:39 +0000

    adding basic styling to patterns index page

--- app/models/difficulty.rb
+++ app/models/difficulty.rb
@@ -1,3 +1,4 @@
 class Difficulty < ActiveRecord::Base
+  has_many :patterns
   attr_accessible :name
 end
--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,5 +1,6 @@
 class Pattern < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
-  attr_accessible :description, :difficulty, :equipment, :instructions, :name, :no_of_rows, :notes, :status, :yarn, :category_id, :user_id
+  belongs_to :difficulty
+  attr_accessible :description, :equipment, :instructions, :name, :no_of_rows, :notes, :status, :yarn, :category_id, :user_id, :difficulty_id
 end
