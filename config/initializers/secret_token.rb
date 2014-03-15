c53f7984328d238a6a855716a35bbe263de134c4
Kriszta <matyi.kriszta@gmail.com>
2014-03-15 12:45:49 +0000

    initial commit, generating new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnittingApp::Application.config.secret_token = '24f7aeb4d6355fd06066842fdba64218518ed7326ec31bd7600189b0d8ffefb952637eab37e3504591ce533c60740d1dc155071bba28d26829e6c9b08b57f44c'
