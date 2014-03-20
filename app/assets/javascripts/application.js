a74a0693bdfb6c67afa4d3afa56fd9c334394646
Kriszta <matyi.kriszta@gmail.com>
2014-03-20 12:46:34 +0000

    adding isotope fluid layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,10 +44,21 @@ $(document).ready(function(){
 
   $("a[rel=leanModal]").leanModal();
 
-  $('#container').isotope({
-  masonry: {
-    columnWidth: 240
-  }
+  var $container = $('#container')
+  // initialize Isotope
+  $container.isotope({
+    // options...
+    resizable: false, // disable normal resizing
+    // set columnWidth to a percentage of container width
+    masonry: { columnWidth: $container.width() / 5 }
+  });
+
+  // update columnWidth on window resize
+  $(window).smartresize(function(){
+    $container.isotope({
+      // update columnWidth to a percentage of container width
+      masonry: { columnWidth: $container.width() / 5 }
+    });
   });
 
 });
